1c5c19fd203f0529beb55cc38c294b8b4b42beb5
samanthaphillips1 <55101428+samanthaphillips1@users.noreply.github.com>
2019-11-06 18:18:39 -0500

    Add files via upload

---
+++ LabExercise3/scripts.js
@@ -0,0 +1,10 @@
+//This will output when the page loads
+document.write("Output Zero Here");
+
+//This will output when a button is pressed
+function theRest(){
+	document.getElementById("output0").innerHTML="0";
+	document.getElementById("output1").innerHTML="1";
+	document.getElementById("output2").innerHTML="2";
+	document.getElementById("output3").innerHTML="3";
+}
